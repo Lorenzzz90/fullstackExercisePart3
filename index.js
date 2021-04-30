e3766406830aa1665d42e5bb4a54c66d58f03728
Lorenzzz90 <lorenzomarziali90@gmail.com>
2021-04-30 20:16:21 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-const app = express() 
+const app = express()
 
 //middleware
 app.use(express.static('build')) //serve ad express per mostrare elementi statici (index.html javascript ecc)
@@ -15,84 +15,80 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 //fine middleware
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result =>{
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-    const opts = {
-         runValidators: true,
-         new: true
-    }
-    Person.findByIdAndUpdate(request.params.id, { number: request.body.number }, opts)
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
+
+  const opts = {
+    runValidators: true,
+    new: true
+  }
+  Person.findByIdAndUpdate(request.params.id, { number: request.body.number }, opts)
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 })
 
 app.get('/info', (request, response) => {
-    Person.countDocuments({}, (err, c) => {
-        console.log('Count is ' + c)
-        console.log(typeof c)
-        response.send(`<div><div>Phonebook has infooo for ${c} people</div><div>${Date()}</div></div>`)
-    })
+  Person.countDocuments({}, (err, c) => {
+    console.log('Count is ' + c)
+    console.log(typeof c)
+    response.send(`<div><div>Phonebook has infooo for ${c} people</div><div>${Date()}</div></div>`)
+  })
 
-}) 
+})
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 //per richieste senza route penultimo middleware
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint'})
-  }
-  
-  app.use(unknownEndpoint)
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 // errorhandler ultimo middleware
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id'})
-    } else if (error.name === 'ValidationError'){
-        return response.status(400).json({ error: error.message})
-    }
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message })
   }
-  
+  next(error)
+}
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require ('mongoose')
 
 if (process.argv.length !==3 && process.argv.length !== 5) {
-    console.log("Wrong input, provide only the password if you want the list of people or <password> <name> <number> if you want to add someone to the phonebook")
-    process.exit(1)
+  console.log('Wrong input, provide only the password if you want the list of people or <password> <name> <number> if you want to add someone to the phonebook')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,27 +14,27 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.gb5jt.mongodb.net/phon
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person =  mongoose.model('Person', personSchema)
 
 if(process.argv.length === 5){
-    const person = new Person({
-        name: name,
-        number: number,
-    })
-    person.save().then(result => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
     })
+    mongoose.connection.close()
+  })
 
 }
